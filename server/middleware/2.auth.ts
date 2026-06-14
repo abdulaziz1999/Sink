66ab67da38a0b5f3a270b06933882224e78663db
Abdul Aziz <36178522+abdulaziz1999@users.noreply.github.com>
2026-06-14 13:51:00 +0700

    Update 2.auth.ts

--- server/middleware/2.auth.ts
+++ server/middleware/2.auth.ts
@@ -1,15 +1,19 @@
 export default eventHandler((event) => {
   const token = getHeader(event, 'Authorization')?.replace(/^Bearer\s+/, '')
   if (event.path.startsWith('/api/') && token !== useRuntimeConfig(event).siteToken) {
+    const config = useRuntimeConfig(event)
+
+    // 2. Jalankan log TERLEBIH DAHULU agar tercatat di Cloudflare
+    console.log("=== DEBUG CLOUDFLARE ENV ===")
+    console.log("Token yang kamu ketik di browser (token):", token)
+    console.log("Token asli dari Header:", getHeader(event, 'authorization'))
+    console.log("Token yang dibaca Nuxt Config (siteToken):", config.siteToken)
+    console.log("Token langsung dari process.env:", process.env.NUXT_SITE_TOKEN)
+    console.log("=============================")
     throw createError({
       status: 401,
       statusText: 'Unauthorized',
     })
-    console.log("=== DEBUG CLOUDFLARE ENV ===")
-    console.log("Token yang kamu ketik di browser:", getHeader(event, 'authorization'))
-    console.log("Token yang dibaca Nuxt Config (siteToken):", config.siteToken)
-    console.log("Token langsung dari process.env:", process.env.NUXT_SITE_TOKEN)
-    console.log("=============================")
   }
   if (token && token.length < 8) {
     throw createError({
